b6adc98b85740730ac4d186ccb0ce89157aa873b
Abhishek kumar singh <abhishekkumarsingh5914@gmail.com>
2024-04-23 11:22:26 +0530

    files

---
+++ src/index.js
@@ -0,0 +1,41 @@
+// mongoose.set(‘strictQuery’, true) 
+// require('dotenv').config({path: './env'})
+import dotenv from "dotenv";
+// Second approach
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path:'./env'
+});
+
+
+connectDB();
+
+
+
+
+/*// first Approach
+import mongoose from "mongoose"
+import {DB_NAME} from "./constants.js"
+
+import express from "express";
+const app = express();
+
+// ife = immediately execute function
+( async () => {
+    try{
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+        app.on("error",(error) => {
+            console.log("Error", error);
+            throw error
+        })
+
+        app.listen(process.env.PORT,()=> {
+            console.log(`Server is listening on port ${process.env.PORT}`)
+        })
+    }catch(error){
+        console.error("ERROR:",error);
+    }
+})()
+
+*/
